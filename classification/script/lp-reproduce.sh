1e5cdecbe90797dbc19de0e384b80af6535386f4
SiyuanYan1 <2546345110m@gmail.com>
2025-04-26 20:33:54 +1000

    fix lp error

--- classification/script/lp-reproduce.sh
+++ classification/script/lp-reproduce.sh
@@ -1,10 +1,11 @@
-models=('PanDerm-Large' 'PanDerm-Base' 'SwAVDerm'  'dinov2' 'imgnet_large21k')
-checkpoints=('/home/syyan/XJ/PanDerm-open_source/pretrain_weight/panderm-large.pth' \
-             '/home/syyan/XJ/PanDerm-open_source/pretrain_weight/panderm-base.pth' \
-             '/home/share/FM_Code/PanDerm/Model_Weights/swavderm_pretrained.pth' \
-             '' \
-             '')
-#models=('imgnet_large21k')
+
+#models=('PanDerm-Large' 'PanDerm-Base' 'SwAVDerm'  'dinov2' 'imgnet_large21k')
+models=('PanDerm-Large')
+#checkpoints=('/home/syyan/XJ/PanDerm-open_source/pretrain_weight/panderm-large.pth' \
+#             '/home/syyan/XJ/PanDerm-open_source/pretrain_weight/panderm-base.pth' \
+#             '/home/share/FM_Code/PanDerm/Model_Weights/swavderm_pretrained.pth' )
+checkpoints=('/home/syyan/XJ/PanDerm-open_source/pretrain_weight/panderm-large.pth')
+
 
 if [ ${#models[@]} -ne ${#checkpoints[@]} ]; then
   echo "Error: models and checkpoints arrays must have the same length"
@@ -26,7 +27,7 @@ for i in "${!models[@]}"; do
       --output_dir "/home/share/FM_Code/FM_Eval/LP_Eval/output_dir2/ID_Res/${model}_res/" \
       --csv_path "/home/share/Uni_Eval/HAM10000_clean/ISIC2018_splits/HAM_clean.csv" \
       --root_path "/home/share/Uni_Eval/HAM10000_clean/ISIC2018/" \
-      --checkpoint "$checkpoint"
+      --pretrained_checkpoint "$checkpoint"
       
     CUDA_VISIBLE_DEVICES=0 python3 linear_eval.py \
       --batch_size 1000 \
@@ -37,7 +38,7 @@ for i in "${!models[@]}"; do
       --output_dir "/home/share/FM_Code/FM_Eval/LP_Eval/output_dir2/ID_Res/${model}_res/" \
       --csv_path '/home/share/Uni_Eval/WSI_patch_data/patch.csv' \
       --root_path '/home/share/Uni_Eval/WSI_patch_data/images/' \
-      --checkpoint "$checkpoint"
+      --pretrained_checkpoint "$checkpoint"
       
     CUDA_VISIBLE_DEVICES=0 python3 linear_eval.py \
       --batch_size 1000 \
@@ -48,7 +49,7 @@ for i in "${!models[@]}"; do
       --output_dir "/home/share/FM_Code/FM_Eval/LP_Eval/output_dir2/ID_Res/${model}_res/" \
       --csv_path '/home/share/Uni_Eval/MSKCC/mskcc.csv' \
       --root_path '/home/share/Uni_Eval/MSKCC/images/' \
-      --checkpoint "$checkpoint"
+      --pretrained_checkpoint "$checkpoint"
       
     CUDA_VISIBLE_DEVICES=1 python3 linear_eval.py \
       --batch_size 1000 \
@@ -59,7 +60,7 @@ for i in "${!models[@]}"; do
       --output_dir "/home/share/FM_Code/FM_Eval/LP_Eval/output_dir2/ID_Res/${model}_res/" \
       --csv_path '/home/share/Uni_Eval/BCN20000/bcn20000.csv' \
       --root_path '/home/share/Uni_Eval/BCN20000/images/' \
-      --checkpoint "$checkpoint"
+      --pretrained_checkpoint "$checkpoint"
 
     CUDA_VISIBLE_DEVICES=2 python3 linear_eval.py \
       --batch_size 1000 \
@@ -70,7 +71,7 @@ for i in "${!models[@]}"; do
       --output_dir "/home/share/FM_Code/FM_Eval/LP_Eval/output_dir2/ID_Res/${model}_res/" \
       --csv_path '/home/share/Uni_Eval/HIBA/hiba.csv' \
       --root_path '/home/share/Uni_Eval/HIBA/images/' \
-      --checkpoint "$checkpoint"
+      --pretrained_checkpoint "$checkpoint"
 
     CUDA_VISIBLE_DEVICES=2 python3 linear_eval.py \
       --batch_size 1000 \
@@ -81,7 +82,7 @@ for i in "${!models[@]}"; do
       --output_dir "/home/share/FM_Code/FM_Eval/LP_Eval/output_dir2/ID_Res/${model}_res/" \
       --csv_path "/home/share/Uni_Eval/pad-ufes/2000.csv" \
       --root_path "/home/share/Uni_Eval/pad-ufes/images/" \
-      --checkpoint "$checkpoint"
+      --pretrained_checkpoint "$checkpoint"
       
     CUDA_VISIBLE_DEVICES=3 python3 linear_eval.py \
       --batch_size 1000 \
@@ -92,7 +93,7 @@ for i in "${!models[@]}"; do
       --output_dir "/home/share/FM_Code/FM_Eval/LP_Eval/output_dir2/ID_Res/${model}_res/" \
       --csv_path "/home/share/Uni_Eval/Derm7pt/atlas-clinical-all.csv" \
       --root_path "/home/share/Uni_Eval/Derm7pt/images/" \
-      --checkpoint "$checkpoint"
+      --pretrained_checkpoint "$checkpoint"
       
     CUDA_VISIBLE_DEVICES=3 python3 linear_eval.py \
       --batch_size 1000 \
@@ -103,7 +104,7 @@ for i in "${!models[@]}"; do
       --output_dir "/home/share/FM_Code/FM_Eval/LP_Eval/output_dir2/ID_Res/${model}_res/" \
       --csv_path '/home/share/Uni_Eval/DDI/ddi_clean.csv' \
       --root_path '/home/share/Uni_Eval/DDI/images/' \
-      --checkpoint "$checkpoint"
+      --pretrained_checkpoint "$checkpoint"
 
     CUDA_VISIBLE_DEVICES=1 python3 linear_eval.py \
       --batch_size 1000 \
@@ -114,7 +115,7 @@ for i in "${!models[@]}"; do
       --output_dir "/home/share/FM_Code/FM_Eval/LP_Eval/output_dir2/task2/${model}_res/" \
       --csv_path "/home/share/Uni_Eval/Dermnet/dermnet.csv" \
       --root_path "/home/share/Uni_Eval/Dermnet/images/" \
-      --checkpoint "$checkpoint"
+      --pretrained_checkpoint "$checkpoint"
   done
   wait
 done
