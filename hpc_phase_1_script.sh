57266c6a3d10bdb9f76c526e6f61ec278c121bfd
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2025-09-28 23:43:08 -0400

    Update phase 1 script

--- hpc_phase_1_script.sh
+++ hpc_phase_1_script.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+#SBATCH --job-name=panderm_phase_1
+#SBATCH --output=logs/panderm_phase_1_%j.out
+#SBATCH --error=logs/panderm_phase_1_%j.err
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 
 # export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 # export LD_LIBRARY_PATH=/usr/local/cuda-11.0/lib64
 
+echo "Starting job on " `date`
+echo "Running on node " `hostname`
 echo $CUDA_VISIBLE_DEVICES
 
 # pip3 install virtualenv
