b62ceff315eda91875836c6d84f01f8d7cbff12c
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2025-10-07 00:43:37 -0400

    Refactor output directory path for phase 1 results and add aggregation script

---
+++ hpc_phase_1_aggregate_k_fold.sh
@@ -0,0 +1,13 @@
+# NOTE - Represents last stage of phase 1; aggregates results from each fold into a single CSV file
+
+PYTHON="python3"
+OUTPUT_DIR="/home/PACE/ja50529n/MS Thesis/Model/PanDerm/output/phase_1" # NOTE - Adjust name according to phase of experiment
+N_SPLITS=10
+CSV_FILENAME="PanDerm_Large_LP_result.csv"
+
+cd classification
+
+${PYTHON} aggregate_results.py \
+    --output_dir "${OUTPUT_DIR}" \
+    --n_splits ${N_SPLITS} \
+    --csv_filename "${CSV_FILENAME}"
