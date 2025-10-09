0080993a59d99b396e9a0ea58c5f775ebf68092e
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2025-10-09 11:11:24 -0400

    Force single GPU mode

--- classification/furnace/utils.py
+++ classification/furnace/utils.py
@@ -332,6 +332,11 @@ def save_on_master(*args, **kwargs):
 
 
 def init_distributed_mode(args):
+    print('Forcing single-GPU mode. Distributed mode is disabled.')
+    args.distributed = False
+    return
+
+
     if args.dist_on_itp:
         args.rank = int(os.environ['OMPI_COMM_WORLD_RANK'])
         args.world_size = int(os.environ['OMPI_COMM_WORLD_SIZE'])
