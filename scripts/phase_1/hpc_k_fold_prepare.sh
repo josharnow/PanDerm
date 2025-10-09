14e0e67d0c4e6000567603b07bdf137bd5df5aaf
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2025-10-09 00:00:15 -0400

    Add k-fold data preparation and HPC finetune scripts

---
+++ scripts/phase_1/hpc_k_fold_prepare.sh
@@ -0,0 +1,31 @@
+# NOTE - Prepares k-fold CSV files for HPC finetune script
+
+PYTHON="python3"
+OUTPUT_DIR="/home/PACE/ja50529n/MS Thesis/Model/PanDerm/output/phase_1" # NOTE - Adjust name according to phase of experiment
+N_SPLITS=10
+
+# # --- Change to the project root directory ---
+cd ../..
+
+# create virtualenv with...
+if [ ! -d venv ]; then
+    python3 -m virtualenv -p python3 venv
+    # install libraries with...
+    venv/bin/pip install -r requirements.txt
+    venv/bin/pip install -r classification/requirements.txt
+    venv/bin/pip install -r segmentation/requirements.txt
+fi
+source venv/bin/activate
+
+mkdir -p ${OUTPUT_BASE_DIR}
+
+
+# Navigate to your classification directory
+cd classification
+
+# Run the preparation script
+${PYTHON} prepare_k_fold_data.py \
+    --csv_path "/home/PACE/ja50529n/MS Thesis/Thesis Data/Skin Cancer Project/PanDerm & SkinEHDLF/ISIC 2024 (SLICE-3D)/ISIC_2024_Training_GroundTruth.csv" \
+    --output_dir "${OUTPUT_DIR}" \
+    --n_splits ${N_SPLITS} \
+    --label_column "binary_label"
