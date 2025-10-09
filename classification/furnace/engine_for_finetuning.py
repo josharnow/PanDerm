7c11d381c5c4c34399284ec32d0e843b6be7c49a
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2025-10-09 02:03:21 -0400

    Add smoothing parameter and flag for weighted random sampler in hpc_finetune.sbatch

--- classification/furnace/engine_for_finetuning.py
+++ classification/furnace/engine_for_finetuning.py
@@ -18,7 +18,6 @@ from sklearn.utils import resample
 
 import torchvision.transforms as transforms
 from torchvision.transforms import functional as TF
-import torch
 import torch.nn.functional as F
 import torch.nn as nn
 
@@ -88,102 +87,106 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
     else:
         optimizer.zero_grad()
 
-    for data_iter_step, (samples, _, targets) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
-        step = data_iter_step // update_freq
-        if step >= num_training_steps_per_epoch:
-            continue
-        it = start_steps + step  # global training iteration
-        # Update LR & WD for the first acc
-        if lr_schedule_values is not None or wd_schedule_values is not None and data_iter_step % update_freq == 0:
-            for i, param_group in enumerate(optimizer.param_groups):
-                if lr_schedule_values is not None:
-                    if "lr_scale" in param_group:
-                        param_group["lr"] = lr_schedule_values[it] * param_group["lr_scale"]
-                    else:
-                        param_group["lr"] = lr_schedule_values[it]
-                if wd_schedule_values is not None and param_group["weight_decay"] > 0:
-                    param_group["weight_decay"] = wd_schedule_values[it]
+    # --- Enable Anomaly Detection ---
+    # This will slow down training but will give a precise error message
+    # pointing to the exact line of code causing the NaN.
+    with torch.autograd.set_detect_anomaly(True):
+        for data_iter_step, (samples, _, targets) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+            step = data_iter_step // update_freq
+            if step >= num_training_steps_per_epoch:
+                continue
+            it = start_steps + step  # global training iteration
+            # Update LR & WD for the first acc
+            if lr_schedule_values is not None or wd_schedule_values is not None and data_iter_step % update_freq == 0:
+                for i, param_group in enumerate(optimizer.param_groups):
+                    if lr_schedule_values is not None:
+                        if "lr_scale" in param_group:
+                            param_group["lr"] = lr_schedule_values[it] * param_group["lr_scale"]
+                        else:
+                            param_group["lr"] = lr_schedule_values[it]
+                    if wd_schedule_values is not None and param_group["weight_decay"] > 0:
+                        param_group["weight_decay"] = wd_schedule_values[it]
 
-        samples = samples.to(device, non_blocking=True)
-        targets = targets.to(device, non_blocking=True)
+            samples = samples.to(device, non_blocking=True)
+            targets = targets.to(device, non_blocking=True)
 
-        if mixup_fn is not None:
-            samples, targets = mixup_fn(samples, targets)
+            if mixup_fn is not None:
+                samples, targets = mixup_fn(samples, targets)
 
-        if loss_scaler is None:
-            samples = samples.half()
-            loss, output = train_class_batch(
-                model, samples, targets, criterion)
-        else:
-            with torch.cuda.amp.autocast():
+            if loss_scaler is None:
+                samples = samples.half()
                 loss, output = train_class_batch(
                     model, samples, targets, criterion)
+            else:
+                with torch.cuda.amp.autocast():
+                    loss, output = train_class_batch(
+                        model, samples, targets, criterion)
 
-        loss_value = loss.item()
+            loss_value = loss.item()
 
-        if not math.isfinite(loss_value):
-            print("Loss is {}, stopping training".format(loss_value))
-            sys.exit(1)
+            if not math.isfinite(loss_value):
+                print("Loss is {}, stopping training".format(loss_value))
+                sys.exit(1)
 
-        if loss_scaler is None:
-            loss /= update_freq
-            model.backward(loss)
-            model.step()
+            if loss_scaler is None:
+                loss /= update_freq
+                model.backward(loss)
+                model.step()
 
-            if (data_iter_step + 1) % update_freq == 0:
-                # model.zero_grad()
-                # Deepspeed will call step() & model.zero_grad() automatic
-                if model_ema is not None:
-                    model_ema.update(model)
-            grad_norm = None
-            loss_scale_value = get_loss_scale_for_deepspeed(model)
-        else:
-            # this attribute is added by timm on one optimizer (adahessian)
-            is_second_order = hasattr(optimizer, 'is_second_order') and optimizer.is_second_order
-            loss /= update_freq
-            grad_norm = loss_scaler(loss, optimizer, clip_grad=max_norm,
-                                    parameters=model.parameters(), create_graph=is_second_order,
-                                    update_grad=(data_iter_step + 1) % update_freq == 0)
-            if (data_iter_step + 1) % update_freq == 0:
-                optimizer.zero_grad()
-                if model_ema is not None:
-                    model_ema.update(model)
-            loss_scale_value = loss_scaler.state_dict()["scale"]
+                if (data_iter_step + 1) % update_freq == 0:
+                    # model.zero_grad()
+                    # Deepspeed will call step() & model.zero_grad() automatic
+                    if model_ema is not None:
+                        model_ema.update(model)
+                grad_norm = None
+                loss_scale_value = get_loss_scale_for_deepspeed(model)
+            else:
+                # this attribute is added by timm on one optimizer (adahessian)
+                is_second_order = hasattr(optimizer, 'is_second_order') and optimizer.is_second_order
+                loss /= update_freq
+                grad_norm = loss_scaler(loss, optimizer, clip_grad=max_norm,
+                                        parameters=model.parameters(), create_graph=is_second_order,
+                                        update_grad=(data_iter_step + 1) % update_freq == 0)
+                if (data_iter_step + 1) % update_freq == 0:
+                    optimizer.zero_grad()
+                    if model_ema is not None:
+                        model_ema.update(model)
+                loss_scale_value = loss_scaler.state_dict()["scale"]
 
-        torch.cuda.synchronize()
+            torch.cuda.synchronize()
 
-        if mixup_fn is None:
-            class_acc = (output.max(-1)[-1] == targets).float().mean()
-        else:
-            class_acc = None
-        metric_logger.update(loss=loss_value)
-        metric_logger.update(class_acc=class_acc)
-        metric_logger.update(loss_scale=loss_scale_value)
-        min_lr = 10.
-        max_lr = 0.
-        for group in optimizer.param_groups:
-            min_lr = min(min_lr, group["lr"])
-            max_lr = max(max_lr, group["lr"])
+            if mixup_fn is None:
+                class_acc = (output.max(-1)[-1] == targets).float().mean()
+            else:
+                class_acc = None
+            metric_logger.update(loss=loss_value)
+            metric_logger.update(class_acc=class_acc)
+            metric_logger.update(loss_scale=loss_scale_value)
+            min_lr = 10.
+            max_lr = 0.
+            for group in optimizer.param_groups:
+                min_lr = min(min_lr, group["lr"])
+                max_lr = max(max_lr, group["lr"])
 
-        metric_logger.update(lr=max_lr)
-        metric_logger.update(min_lr=min_lr)
-        weight_decay_value = None
-        for group in optimizer.param_groups:
-            if group["weight_decay"] > 0:
-                weight_decay_value = group["weight_decay"]
-        metric_logger.update(weight_decay=weight_decay_value)
-        metric_logger.update(grad_norm=grad_norm)
+            metric_logger.update(lr=max_lr)
+            metric_logger.update(min_lr=min_lr)
+            weight_decay_value = None
+            for group in optimizer.param_groups:
+                if group["weight_decay"] > 0:
+                    weight_decay_value = group["weight_decay"]
+            metric_logger.update(weight_decay=weight_decay_value)
+            metric_logger.update(grad_norm=grad_norm)
 
-        if log_writer is not None:
-            log_writer.update(loss=loss_value, head="loss")
-            log_writer.update(class_acc=class_acc, head="loss")
-            log_writer.update(loss_scale=loss_scale_value, head="opt")
-            log_writer.update(lr=max_lr, head="opt")
-            log_writer.update(min_lr=min_lr, head="opt")
-            log_writer.update(weight_decay=weight_decay_value, head="opt")
-            log_writer.update(grad_norm=grad_norm, head="opt")
+            if log_writer is not None:
+                log_writer.update(loss=loss_value, head="loss")
+                log_writer.update(class_acc=class_acc, head="loss")
+                log_writer.update(loss_scale=loss_scale_value, head="opt")
+                log_writer.update(lr=max_lr, head="opt")
+                log_writer.update(min_lr=min_lr, head="opt")
+                log_writer.update(weight_decay=weight_decay_value, head="opt")
+                log_writer.update(grad_norm=grad_norm, head="opt")
 
-            log_writer.set_step()
+                log_writer.set_step()
 
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
